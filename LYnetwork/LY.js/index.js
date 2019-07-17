380a17de65d00d18bcfa8a4a66f7442a5c17a9bc
Hhua <3434@qq.com>
2019-07-17 23:05:28 +0800

    666

---
+++ LYnetwork/LY.js/index.js
@@ -0,0 +1,12 @@
+/*  导航栏双态按钮  */ 
+$("#content>ul.aa").click(function(e){
+    var $a=$(e.target);
+    if($a.is(".nav-link")){
+      $a.addClass("act-a");
+      $a.parent().siblings().children().removeClass("act-a");
+    }
+  })
+/*   title动画效果   */
+  $(window).load(function(){
+    $("div.title").addClass("aav")
+})
