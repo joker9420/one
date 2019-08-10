e70f55da9f0c7d75b402b0ea983a22ceb4443bd7
Hhua <3434@qq.com>
2019-08-10 18:39:15 +0800

    ...

--- LYnetwork/LY.js/index.js
+++ LYnetwork/LY.js/index.js
@@ -6,6 +6,21 @@ $("#content>ul.aa").click(function(e){
       $a.parent().siblings().children().removeClass("act-a");
     }
   })
+// 关于 导航栏双态按钮
+  $("#content>ul.bb").click(function(e){
+    var $a=$(e.target);
+    if($a.is(".xuan")){
+    $a.addClass("act-b");
+    $a.parent().siblings().children().removeClass("act-a");
+    }
+})
+$("#content>ul.cc").click(function(e){
+    var $a=$(e.target);
+    if($a.is(".lis")){
+    $a.addClass("act-c");
+    $a.parent().siblings().children().removeClass("act-a");
+    }
+})
 /*   title动画效果   */
   $(window).load(function(){
     $("div.title").addClass("aav")
